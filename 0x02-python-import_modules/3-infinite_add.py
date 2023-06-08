a7ffe63a62e145a9749405781acdb2c7b01a5708
HoussamMrabet <dodslim100@gmail.com>
2023-06-08 16:47:15 +0100

    fix task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-	add += int(s)
-print("{:d}".format(add))
+if __name__ == '__main__':
+    from sys import argv
+    numb = 0
+    for index in argv[1:]:
+        numb += int(index)
+    print('{:d}'.format(numb))
