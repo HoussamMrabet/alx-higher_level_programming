a00666a09a165068d9ec50173fc381f88baf0275
HoussamMrabet <dodslim100@gmail.com>
2023-06-26 11:52:22 +0100

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
@@ -8,5 +9,4 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         None
     print()
-    
     return i
