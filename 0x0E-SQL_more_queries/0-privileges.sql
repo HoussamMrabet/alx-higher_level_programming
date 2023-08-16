83a797289b2768c1423778a91d83965fd5e0f878
HoussamMrabet <dodslim100@gmail.com>
2023-08-16 14:43:25 +0100

    0. My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,7 @@
--- Lists all privileges of the user user_0d_1.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Lists all privileges of the user user_0d_2.
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+SELECT
+    CONCAT("Grants for ", QUOTE(user), "@", QUOTE(host)) AS Grants
+FROM
+    mysql.user
+WHERE
+    user IN ('user_0d_1', 'user_0d_2');
